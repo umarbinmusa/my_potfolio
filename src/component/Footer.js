7ad000de497dd0a0e4fcbddb1da2a61547555043
umarbinmusa <umarmusa20002@gmail.com>
2025-05-04 16:49:17 +0100

    pdate

---
+++ src/component/Footer.js
@@ -0,0 +1,33 @@
+export default function Footer() {
+    return (
+      
+
+<nav class=" ">
+  //
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+   
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 b  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  0 md:dark:bg-gray-900 dark:border-gray-700">
+        
+    
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Portfolio</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">home</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    );
+  }
+  
---
+++ src/component/Nav.js
@@ -0,0 +1,32 @@
+export default function Nav() {
+    return (
+      
+
+<nav class=" ">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+   <h1 className="text-white from-neutral-100 text-2xl">Umar Musa</h1>
+   
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 b  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  0 md:dark:bg-gray-900 dark:border-gray-700">
+        
+    
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Expirience</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Project</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-2xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    );
+  }
+  
