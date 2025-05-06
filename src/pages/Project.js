f7fb90b5fc4c44603a70394b6d60db9fb8d454cb
umarbinmusa <umarmusa20002@gmail.com>
2025-05-06 23:36:23 +0100

    changes

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,6 +1,8 @@
+import Nav from "../component/Nav";
 function project(){
     return(
-        <div className="flex items-center min-h-screen m-20 px-10 bg-slate-950">
+        <div className=" items-center min-h-screen m-20 px-10 bg-slate-950">
+             <Nav />
             <div className="flex flex-row space-x-16">
                 <div className=" bg-white rounded-md"> 
                 <img src="./images/azandash.png" className="p-5" alt="" />
@@ -38,14 +40,26 @@ function project(){
                 </div>
         
                 </div>
-                <div>
-                <img src="./images/git.png" className=" " alt="" />
+                <div className=" bg-white rounded-md"> 
+                <img src="./images/mbjj.png" className="p-5" alt="" />
+                <div className="flex flex-row space-x-14 py-2">
+                    <div className="">
+                    <button class="bg-transparent hover:bg-blue- ml-3 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Github
+          </button>
+        
+                    </div>
+                    <div className="">
+                    <button class="bg-transparent hover:bg-blue- ml-3 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Live
+          </button>
+        
+                        </div>
+                </div>
         
                 </div>
-                <div>
-                <img src="./images/git.png" className=" " alt="" />
-        
-                </div>
+               
+               
 
             </div>
 
